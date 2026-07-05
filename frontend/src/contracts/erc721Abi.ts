29c8d81811b0981dfca382c247c6c19913d56187
VicomteBdV <antoinebdv@gmail.com>
2026-07-05 14:06:51 +0200

    Add NFT metadata previews

--- frontend/src/contracts/erc721Abi.ts
+++ frontend/src/contracts/erc721Abi.ts
@@ -1,6 +1,27 @@
 export const erc721Abi = [
   {
     type: "function",
+    name: "name",
+    stateMutability: "view",
+    inputs: [],
+    outputs: [{ name: "", type: "string" }]
+  },
+  {
+    type: "function",
+    name: "symbol",
+    stateMutability: "view",
+    inputs: [],
+    outputs: [{ name: "", type: "string" }]
+  },
+  {
+    type: "function",
+    name: "tokenURI",
+    stateMutability: "view",
+    inputs: [{ name: "tokenId", type: "uint256" }],
+    outputs: [{ name: "", type: "string" }]
+  },
+  {
+    type: "function",
     name: "ownerOf",
     stateMutability: "view",
     inputs: [{ name: "tokenId", type: "uint256" }],
